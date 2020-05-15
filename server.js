b290590068137d8a38e30ea96d9333bf0a72c4d3
viktorChubyrka <viktor2000897@gmail.com>
2020-05-15 14:25:41 +0300

    comit3

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get(/.*/, function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.VUE_APP_PORT || 5000;
 app.listen(port);
 console.log("server started " + port);
